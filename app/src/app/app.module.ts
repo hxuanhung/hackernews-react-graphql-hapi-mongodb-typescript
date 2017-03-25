f075aa2eace30af6b5560920dd4e9f9d330ffa26
Hung HOANG <hung.xuan@kellydeli.com>
2017-03-25 23:38:45 +0100

    feat(app): add apollo client

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {
-  NgModule,
-  ApplicationRef
+    NgModule,
+    ApplicationRef
 } from '@angular/core';
 import {
-  removeNgStyles,
-  createNewHosts,
-  createInputTransfer
+    removeNgStyles,
+    createNewHosts,
+    createInputTransfer
 } from '@angularclass/hmr';
 import {
-  RouterModule,
-  PreloadAllModules
+    RouterModule,
+    PreloadAllModules
 } from '@angular/router';
 
 /*
@@ -31,89 +31,101 @@ import { XLargeDirective } from './home/x-large';
 import { GifModule } from './gif';
 
 import { MaterialModule } from '@angular/material';
-
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
 import '../styles/styles.scss';
 import '../styles/headings.css';
 
 // Application wide providers
 const APP_PROVIDERS = [
-  ...APP_RESOLVER_PROVIDERS,
-  AppState
+    ...APP_RESOLVER_PROVIDERS,
+    AppState
 ];
 
+const client = new ApolloClient({
+    networkInterface: createNetworkInterface({
+        uri: 'http://localhost:3001/graphql'
+    }),
+});
+
+export function provideClient(): ApolloClient {
+    return client;
+}
+
 type StoreType = {
-  state: InternalStateType,
-  restoreInputValues: () => void,
-  disposeOldHosts: () => void
+    state: InternalStateType,
+    restoreInputValues: () => void,
+    disposeOldHosts: () => void
 };
 
 /**
  * `AppModule` is the main entry point into Angular2's bootstraping process
  */
 @NgModule({
-  bootstrap: [ AppComponent ],
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    HomeComponent,
-    NoContentComponent,
-    XLargeDirective
-  ],
-  imports: [ // import Angular's modules
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-		GifModule,
-		MaterialModule.forRoot(),
-    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
-  ],
-  providers: [ // expose our Services and Providers into Angular's dependency injection
-    ENV_PROVIDERS,
-    APP_PROVIDERS
-  ]
+    bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        HomeComponent,
+        NoContentComponent,
+        XLargeDirective
+    ],
+    imports: [ // import Angular's modules
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        GifModule,
+        MaterialModule.forRoot(),
+        ApolloModule.forRoot(provideClient),
+        RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
+    ],
+    providers: [ // expose our Services and Providers into Angular's dependency injection
+        ENV_PROVIDERS,
+        APP_PROVIDERS
+    ]
 })
 export class AppModule {
 
-  constructor(
-    public appRef: ApplicationRef,
-    public appState: AppState
-  ) {}
+    constructor(
+        public appRef: ApplicationRef,
+        public appState: AppState
+    ) { }
 
-  public hmrOnInit(store: StoreType) {
-    if (!store || !store.state) {
-      return;
+    public hmrOnInit(store: StoreType) {
+        if (!store || !store.state) {
+            return;
+        }
+        console.log('HMR store', JSON.stringify(store, null, 2));
+        // set state
+        this.appState._state = store.state;
+        // set input values
+        if ('restoreInputValues' in store) {
+            let restoreInputValues = store.restoreInputValues;
+            setTimeout(restoreInputValues);
+        }
+
+        this.appRef.tick();
+        delete store.state;
+        delete store.restoreInputValues;
     }
-    console.log('HMR store', JSON.stringify(store, null, 2));
-    // set state
-    this.appState._state = store.state;
-    // set input values
-    if ('restoreInputValues' in store) {
-      let restoreInputValues = store.restoreInputValues;
-      setTimeout(restoreInputValues);
+
+    public hmrOnDestroy(store: StoreType) {
+        const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
+        // save state
+        const state = this.appState._state;
+        store.state = state;
+        // recreate root elements
+        store.disposeOldHosts = createNewHosts(cmpLocation);
+        // save input values
+        store.restoreInputValues = createInputTransfer();
+        // remove styles
+        removeNgStyles();
     }
 
-    this.appRef.tick();
-    delete store.state;
-    delete store.restoreInputValues;
-  }
-
-  public hmrOnDestroy(store: StoreType) {
-    const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
-    // save state
-    const state = this.appState._state;
-    store.state = state;
-    // recreate root elements
-    store.disposeOldHosts = createNewHosts(cmpLocation);
-    // save input values
-    store.restoreInputValues  = createInputTransfer();
-    // remove styles
-    removeNgStyles();
-  }
-
-  public hmrAfterDestroy(store: StoreType) {
-    // display new elements
-    store.disposeOldHosts();
-    delete store.disposeOldHosts;
-  }
+    public hmrAfterDestroy(store: StoreType) {
+        // display new elements
+        store.disposeOldHosts();
+        delete store.disposeOldHosts;
+    }
 
 }
--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,37 +1,54 @@
 import {
-  Component,
-  OnInit
+    Component,
+    OnInit
 } from '@angular/core';
 
 import { AppState } from '../app.service';
 import { Title } from './title';
-import { XLargeDirective } from './x-large';
+
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const GetAllGif = gql`
+  query getAllGif {
+      getAll{
+           _id
+        text
+  }
+}
+`;
 
 @Component({
-  selector: 'home',
-  providers: [
-    Title
-  ],
-  styleUrls: [ './home.component.css' ],
-  templateUrl: './home.component.html'
+    selector: 'home',
+    providers: [
+        Title
+    ],
+    styleUrls: ['./home.component.css'],
+    templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-  // Set our default values
-  public localState = { value: '' };
-  // TypeScript public modifiers
-  constructor(
-    public appState: AppState,
-    public title: Title
-  ) {}
+    // Set our default values
+    public localState = { value: '' };
+    // TypeScript public modifiers
+    constructor(
+        public appState: AppState,
+        public title: Title,
+        private apollo: Apollo
+    ) { }
 
-  public ngOnInit() {
-    console.log('hello `Home` component');
-    // this.title.getData().subscribe(data => this.data = data);
-  }
+    public ngOnInit() {
+        console.log('hello `Home` component');
+        // this.title.getData().subscribe(data => this.data = data);
+        this.apollo.watchQuery({
+            query: GetAllGif
+        }).subscribe(({ data }) => {
+            console.log('Get data', data);
+        });
+    }
 
-  public submitState(value: string) {
-    console.log('submitState', value);
-    this.appState.set('value', value);
-    this.localState.value = '';
-  }
+    public submitState(value: string) {
+        console.log('submitState', value);
+        this.appState.set('value', value);
+        this.localState.value = '';
+    }
 }

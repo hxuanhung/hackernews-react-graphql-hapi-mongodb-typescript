c1c845f301c755738b12430192e5f19cfac17977
hxhung <hung.xuan@kellydeli.com>
2017-03-28 11:05:30 +0200

    feat(app): set fetchPolicy

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {
-    Component,
-    OnInit
+	Component,
+	OnInit
 } from '@angular/core';
 
 import { AppState } from '../app.service';
@@ -20,40 +20,43 @@ const GetAllGif = gql`
 `;
 
 @Component({
-    selector: 'home',
-    providers: [
-        Title
-    ],
-    styleUrls: ['./home.component.css'],
-    templateUrl: './home.component.html'
+	selector: 'home',
+	providers: [
+		Title
+	],
+	styleUrls: ['./home.component.css'],
+	templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-    // Set our default values
-    public localState = { value: '' };
-    gifs: any[];
-    // TypeScript public modifiers
-    constructor(
-        public appState: AppState,
-        public title: Title,
-        private apollo: Apollo
-    ) { }
+	// Set our default values
+	public localState = { value: '' };
+	gifs: any[];
+	// TypeScript public modifiers
+	constructor(
+		public appState: AppState,
+		public title: Title,
+		private apollo: Apollo
+	) { }
 
-    public ngOnInit() {
-        console.log('hello `Home` component');
-        // this.title.getData().subscribe(data => this.data = data);
-        this.apollo.watchQuery({
-            query: GetAllGif
-        })
-            .map(data => data.data)
-            .subscribe(data => {
-                console.log('Get data', data.getAll);
-                this.gifs = data.getAll;
-            });
-    }
+	public ngOnInit() {
+		console.log('hello `Home` component');
+		// this.title.getData().subscribe(data => this.data = data);
+		this.apollo.watchQuery(
+			{
+				query: GetAllGif,
+				fetchPolicy: "network-only"
+			}
+		)
+			.map(data => data.data)
+			.subscribe(data => {
+				console.log('Get data', data.getAll);
+				this.gifs = data.getAll;
+			});
+	}
 
-    public submitState(value: string) {
-        console.log('submitState', value);
-        this.appState.set('value', value);
-        this.localState.value = '';
-    }
+	public submitState(value: string) {
+		console.log('submitState', value);
+		this.appState.set('value', value);
+		this.localState.value = '';
+	}
 }

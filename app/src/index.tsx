4850752a0d69d51ea3f91ac6f9e8250a0a13b342
hxuanhung <hxuanhung@gmail.com>
2017-07-13 15:55:40 +0200

    remove all unused codes

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,7 +4,6 @@ import { ApolloProvider } from 'react-apollo';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Router, Switch } from 'react-router-dom';
-import { App } from './containers/App';
 import FeedPage from './containers/FeedPage';
 import NewUrlPage from './containers/NewUrlPage';
 import { client } from './graphql/config';
@@ -22,7 +21,6 @@ ReactDOM.render(
       <Router history={history}>
         <div>
           <Route path='/' component={Layout} />
-          <Route path='/app' component={App} />
           <Route path='/add' component={NewUrlPage} />
             <Route path='/feed' component={FeedPage} />
         </div>
--- app/src/reducers/index.ts
+++ app/src/reducers/index.ts
@@ -1,12 +1,9 @@
 import { combineReducers, Reducer } from 'redux';
 import { client } from '../graphql/config';
-import todos from './todos';
 export interface IRootState {
-  todos: TodoStoreState;
   apollo: any;
 }
 
 export default combineReducers<IRootState>({
-  // apollo: client.reducer(),
-  todos,
+  apollo: client.reducer(),
 });
